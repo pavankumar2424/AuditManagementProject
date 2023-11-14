6252519da524afd6434babe73d0e4c7a3c469137
pavankumar2424 <100656995+pavankumar2424@users.noreply.github.com>
2023-11-14 15:18:15 +0530

    Add files via upload

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'sneka', 'sn', 'sn000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Rajashri', 'rs', 'rs000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Annie', 'an', 'an000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Pavan Kumar', 'pk', 'pk000', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'pranavan', 'pr', 'pr000', 'Project-5');
